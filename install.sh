1bcb79189b91647c4d3ff858d354c89a17d51e68
Jordan VanderZwaag <jvanderzwaag@narrativescience.com>
2017-06-06 16:58:11 -0500

    Add initial install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install build-essential libssl-dev
+sudo apt-get install nodejs
+sudo apt-get install npm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
